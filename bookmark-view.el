6fd85361c50a91a84d2781bdd2b240f9be84ff4b
Daniel Mendler <mail@daniel-mendler.de>
2021-10-12 15:24:23 +0200

    Expand documentation

--- bookmark-view.el
+++ bookmark-view.el
@@ -24,7 +24,9 @@
 
 ;;; Commentary:
 
-;; Bookmark views
+;; This package provides commands which allow storing the window
+;; configuration (called a view) as an Emacs bookmark. This way the view
+;; is automatically persisted and can be restored after restart.
 
 ;;; Code:
 
